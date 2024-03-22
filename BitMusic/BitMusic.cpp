41bac4f75a298b4cd0a2755a8fa4b3f6c42fea55
qwerty2024 <multanmgu@gmail.com>
2024-03-22 14:32:37 +0300

    Seven nation army beep))).

--- BitMusic/BitMusic.cpp
+++ BitMusic/BitMusic.cpp
@@ -1,29 +1,28 @@
 ﻿#include <stdio.h>
 #include <stdint.h>
 #include <windows.h>
-#include <time.h>
 
-//void playMelody(int frequency[], int duration[], int melodyLenght)
-//{
-//    for (int i = 0; i < melodyLenght; i++)
-//        Beep(frequency[i], duration[i]);
-//}
+void playMelody(int frequency[], int duration[], int melodyLenght)
+{
+    for (int i = 0; i < melodyLenght; i++)
+        Beep(frequency[i], duration[i]);
+}
 
 int main()
 {
-    float frequency[] = { 130.8, 146.8, 164.8, 174.6, 196, 220, 246.9 }; // гамма до-мажор
-    int duration[] = { 60, 90, 120, 180, 240, 360, 480 };
+    int sevenNationArmyFrequence[] = {
+        220, 220, 262.6, 220, 196, 174.6, 164.8,
+        220, 220, 262.6, 220, 196, 174.6, 196, 174.6, 164.8
+    };
 
-    srand(time(NULL)); // текущее время в качестве начального значения
-    while (1) {
-        //генерация псевдослучайных чисел
-        int i = rand() % 7; // номер элемента для частоты (0..6)
-        int j = rand() % 7; // номер элемента для длительности (0..6)
-        int k = rand() % 6 + 1; // коэффициент для частоты от (1..6)
-        int g = rand() % 2 + 1; // коэффициент для длительности (1..2)
-        system("cls");
-        printf("frequency: %.2f\tduration: %d", frequency[i] * k, duration[j] * g);
-        Beep(frequency[i] * k, duration[j] * g);
-    }
+    int sevenNationArmyDuration[] = {
+        750, 250, 333, 333, 333, 1000, 1000,
+        750, 250, 333, 333, 333, 333, 333, 333, 1000
+    };
+
+    playMelody(sevenNationArmyFrequence, sevenNationArmyDuration,
+        sizeof(sevenNationArmyFrequence) / sizeof(sevenNationArmyDuration[0]));
+
+
     return 0;
 }
